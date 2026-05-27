eb0fc000cb7ea291f05401ffe55fd7d4028e1878
AruzeKQ <khaiquang0103@gmail.com>
2026-05-28 00:16:40 +0700

    changedCss for ProductGrid

--- src/components/product/ProductGrid.jsx
+++ src/components/product/ProductGrid.jsx
@@ -1,8 +1,44 @@
 import ProductCard from "./ProductCard"
+import './ProductGrid.css'
 
 const mockData = [
     {
         id: "01",
+        name: "Trendy Queen Womens Off the Shoulder Tops Oversized Summer Spring T-Shirts 2026",
+        img: "https://m.media-amazon.com/images/I/61GXXIx+SgL._AC_UL320_.jpg",
+        price: "300.000",
+        quantity: 1,
+    },
+    {
+        id: "02",
+        name: "Trendy Queen Womens Off the Shoulder Tops Oversized Summer Spring T-Shirts 2026",
+        img: "https://m.media-amazon.com/images/I/61GXXIx+SgL._AC_UL320_.jpg",
+        price: "300.000",
+        quantity: 1,
+    },
+    {
+        id: "03",
+        name: "Trendy Queen Womens Off the Shoulder Tops Oversized Summer Spring T-Shirts 2026",
+        img: "https://m.media-amazon.com/images/I/61GXXIx+SgL._AC_UL320_.jpg",
+        price: "300.000",
+        quantity: 1,
+    },
+    {
+        id: "04",
+        name: "Trendy Queen Womens Off the Shoulder Tops Oversized Summer Spring T-Shirts 2026",
+        img: "https://m.media-amazon.com/images/I/61GXXIx+SgL._AC_UL320_.jpg",
+        price: "300.000",
+        quantity: 1,
+    },
+    {
+        id: "05",
+        name: "Trendy Queen Womens Off the Shoulder Tops Oversized Summer Spring T-Shirts 2026",
+        img: "https://m.media-amazon.com/images/I/61GXXIx+SgL._AC_UL320_.jpg",
+        price: "300.000",
+        quantity: 1,
+    },
+    {
+        id: "06",
         name: "Trendy Queen Womens Off the Shoulder Tops Oversized Summer Spring T-Shirts 2026",
         img: "https://m.media-amazon.com/images/I/61GXXIx+SgL._AC_UL320_.jpg",
         price: "300.000",
@@ -14,7 +50,7 @@ const mockData = [
 export default function ProductGrid() {
 
     return (
-        <div>
+        <div className="product-list">
             {mockData.length > 0 && mockData.map((product) =>
                 <ProductCard key={product.id} product={product} />
             )}
